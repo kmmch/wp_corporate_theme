4468516d8918ccb63592204cfc072782a0f6b230
kmmch <kmmch.tech@gmail.com>
2023-12-05 10:45:44 +0900

    検索メイン画像

--- functions.php
+++ functions.php
@@ -27,6 +27,8 @@ function get_main_title() {
         return get_the_title(  );
     } elseif(is_category(  )) {
         return single_cat_title(  );
+    } elseif(is_search(  )) {
+        return 'サイト内検索結果';
     }
 
     return '';
@@ -81,8 +83,10 @@ function get_main_image() {
         return get_the_post_thumbnail( get_queried_object(  )->ID, 'detail' );
     } elseif (is_category(  ) || is_singular( 'post' )) {
         return '<img src="'. get_template_directory_uri(  ) . '/assets/images/bg-page-news.jpg" />';
+    } elseif (is_search(  )) {
+        return '<img src="'. get_template_directory_uri(  ) . '/assets/images/bg-page-search.jpg" />';
     } else {
-        return '<img src="'. get_template_directory_uri(  ) . '/assets/images/bg-page-dummy.jpg" />';
+        return '<img src="'. get_template_directory_uri(  ) . '/assets/images/bg-page-dummy.png" />';
     }
 }
 
